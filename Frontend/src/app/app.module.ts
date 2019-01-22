9adc95347b58a32eace2127cb35276c08f3e344a
ahme <ahmouckerick@gmail.Com>
2019-01-22 14:58:17 +0100

    add header and routes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { LearningComponent } from './learning/learning.component';
 import { PredictionComponent } from './prediction/prediction.component';
 import { InteractionGraphComponent } from './interaction-graph/interaction-graph.component';
 import { ModelComponent } from './model/model.component';
+import { CompteComponent } from './compte/compte.component';
+import { DonneesComponent } from './donnees/donnees.component';
+import { HistoriqueComponent } from './historique/historique.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { ModelComponent } from './model/model.component';
     LearningComponent,
     PredictionComponent,
     InteractionGraphComponent,
-    ModelComponent
+    ModelComponent,
+    CompteComponent,
+    DonneesComponent,
+    HistoriqueComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
