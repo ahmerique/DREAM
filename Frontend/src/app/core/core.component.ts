3a1a4265a471b6c6005d55cab6b23e659b06247c
taghurei <gauthier.lacroix@supelec.fr>
2019-01-23 11:01:07 +0100

    css

--- Frontend/src/app/core/core.component.ts
+++ Frontend/src/app/core/core.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoreComponent implements OnInit {
   hauteur = 200;
-
+  selectedDevice: String;
   constructor() { }
+  addfile: boolean = false;
+  addGraph: boolean = false;
 
   ngOnInit() {
   }
+  Addfile() {
+    if (!this.addfile) {
+      this.addfile = true;
+    }
+    else {
+      this.addfile = false;
 
+    }
+  }
+  Addgraph() {
+    if (!this.addGraph) {
+      this.addGraph = true;
+    }
+    else {
+      this.addGraph = false;
+
+    }
+  }
+  test(arg) {
+    console.log(arg);
+  }
 }
