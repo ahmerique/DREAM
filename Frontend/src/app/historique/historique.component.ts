e4995b9dc3673025fe5b4ac5fefab12107752af4
ahme <ahmouckerick@gmail.Com>
2019-01-23 17:47:28 +0100

    fin de journée

--- Frontend/src/app/historique/historique.component.ts
+++ Frontend/src/app/historique/historique.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoriqueComponent implements OnInit {
 
+  _historique = ['modèle1', 'modèle2'];
+
   constructor() { }
 
   ngOnInit() {
