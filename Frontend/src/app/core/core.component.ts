7069650c0493c504bef3f58b900467f64d721b53
ahme <ahmouckerick@gmail.Com>
2019-01-28 13:58:46 +0100

    graph depend on file chosen

--- Frontend/src/app/core/core.component.ts
+++ Frontend/src/app/core/core.component.ts
@@ -25,6 +25,13 @@ export class CoreComponent implements OnInit {
   selectedGraphType = 'temporelle';
   chart: Chart;
 
+  data1 = [{ label: 'G1', data: [1, 2, 3] }, { label: 'G2', data: [1, 3, 2] }, { label: 'G3', data: [3, 2, 1] }];
+  data2 = [{ label: 'G1', data: [5, 3, 1] }, { label: 'G2', data: [1, 2, 3] }, { label: 'G3', data: [1, 3, 2] }];
+  data3 = [{ label: 'G2', data: [5, 3, 1] }, { label: 'G3', data: [1, 10, 3] }, { label: 'G1', data: [1, 3, 2] }];
+  dataGraph = [this.data1, this.data2, this.data3];
+  xAxis = [0, 1, 2];
+
+
   options = [
     { name: 'option1', value: 1 },
     { name: 'option2', value: 2 }
@@ -32,30 +39,10 @@ export class CoreComponent implements OnInit {
 
   ngOnInit() {
     this.chart = new Chart('myChart', {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: this.selectedGraphType,
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
+        labels: this.xAxis,
+        datasets: this.dataGraph[0]
       },
       options: {
         scales: {
@@ -67,19 +54,20 @@ export class CoreComponent implements OnInit {
         },
         title: {
           display: true,
-          text: this.data[this.selectedOption].name
+          text: this.data[this.selectedOption].name + ' ' + this.selectedGraphType
         }
       }
     });
   }
 
   changeGraphType() {
-    this.chart.data.datasets[0].label = this.selectedGraphType;
+    this.chart.options.title.text = this.data[this.selectedOption].name + ' ' + this.selectedGraphType;
     this.chart.update();
   }
 
   changeOption() {
-    this.chart.options.title.text = this.data[this.selectedOption].name;
+    this.chart.options.title.text = this.data[this.selectedOption].name + ' ' + this.selectedGraphType;
+    this.chart.data.datasets = this.dataGraph[this.selectedOption];
     this.chart.update();
   }
 
